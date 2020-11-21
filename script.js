0646ae6921c95ea2c153638569537f84f6edb7e2
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-21 00:38:32 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ button.addEventListener('click', function () {
   const emailTel = document.querySelector('#user-email-phone').value;
   alert(emailTel);
 });
+
+const divErro = document.querySelector('.erro');
+// let div = document.createElement('div');
+
+const dados = document.querySelectorAll('.form');
+const button2 = document.querySelector('#facebook-register');
+
+button2.addEventListener('click', function (event) {
+  for (let index = 0; index < dados.length; index += 1) {
+    if (dados[index].value === '') {
+      event.preventDefault();
+      divErro.innerText = 'erro';
+    }
+  }
+});
