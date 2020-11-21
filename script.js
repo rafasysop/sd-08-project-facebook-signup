5e6668b381049ef683ce433dd0fe11324ae9dcd5
WagnerMor <wagnermor@outlook.com>
2020-11-21 01:16:49 -0300

    requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ button.addEventListener('click', function () {
   const emailTel = document.querySelector('#user-email-phone').value;
   alert(emailTel);
 });
+function validaCamposVazios() {
+  const bntRegister = document.getElementById('facebook-register');
+  bntRegister.addEventListener('click', (event) => {
+    const inputs = document.querySelectorAll('.valida');
+    const formulario = document.querySelector('.right-content form');
+    for (let index = 0; index < inputs.length; index += 1) {
+      if(inputs[index].value === '' || inputs[index].value === ' ') {
+        event.preventDefault();// para o button nao dar reload na pagina
+        const camposInvalidos = document.createElement('p');
+        camposInvalidos.innerText = 'Campos inválidos';
+        formulario.appendChild(camposInvalidos);
+        break
+      }
+    }
+  });
+}validaCamposVazios();
